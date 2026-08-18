04f39bdb16514a8671b521cc6b992ebaf5ee445b
gauravkhosla0604 <gauravkhosla0604@gmail.com>
2026-08-19 00:44:09 +0530

    Time: 51 ms (79.21%), Space: 13.8 MB (85.04%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+
+        for(string s:strs){
+            int zeroes=0;
+            int ones=0;
+
+            for(char ch:s){
+                if(ch=='0'){
+                    zeroes++;
+                }else{
+                    ones++;
+                }
+            }
+            for(int i=m;i>=zeroes;i--){
+                for(int j=n;j>=ones;j--){
+                    dp[i][j]=max(dp[i][j],dp[i-zeroes][j-ones]+1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
