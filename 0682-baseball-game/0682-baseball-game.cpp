68568a4d48c8b5325ba14c064a5b7ffb6d85ba52
gauravkhosla0604 <gauravkhosla0604@gmail.com>
2026-08-25 21:57:18 +0530

    Time: 0 ms (100%), Space: 11.9 MB (82.56%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> st;
+
+        for(string op:operations){
+            if(op=="C"){
+                st.pop_back();
+            }else if(op=="D"){
+                st.push_back(2*st.back());
+            }else if(op=="+"){
+                int n=st.size();
+                st.push_back(st[n-1]+st[n-2]);
+            }else{
+                st.push_back(stoi(op));
+            }
+        }
+        int ans=0;
+
+        for(int x:st){
+            ans+=x;
+        }
+        return ans;
+    }
+};
